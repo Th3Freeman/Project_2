030bcd2de2fa4821e85dd277c7111fd992a3342b
Dan Shelly <djshelly24@gmail.com>
2019-12-02 14:23:17 -0500

    Adding beginning of server outline

---
+++ server.js
@@ -0,0 +1,20 @@
+//Dependencies
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+//Setting up port
+var PORT = process.env.PORT || 8080;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+
+
+
+//Listening function
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
