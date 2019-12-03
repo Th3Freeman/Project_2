fffd51a0eab725faae64e02327123fe05c924c97
cbush19 <cbush5400@gmail.com>
2019-12-03 14:47:48 -0500

    added db models

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const exphbs = require("express-handlebars");
 const session = ("express-session")
 const path = require("path");
 //Requiring our passport configuration
-var passport = require("./config/passport");
+//var passport = require("./config/passport");
 var passport = require("./models")
 
 //Setting up port
@@ -17,15 +17,18 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Uses sessions to keep track of our user's login status
-app.use(session({ secret: "MrSweetMeats", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(session({ secret: "MrSweetMeats", resave: true, saveUninitialized: true }));
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 app.use(express.static('public'))
 
 
 
-//Listening function
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
+// Synchronize my schema
+db.sequelize.sync({ force: process.env.NODE_ENV !== "production" })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`==> Server listening at http://localhost:${PORT}/`);
+    });
+  });
