0d5eed5eb78688b38f117c5bb335342d332c8d50
Dan Shelly <djshelly24@gmail.com>
2019-12-03 13:34:57 -0500

    Converting html files to handlebars

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const exphbs = require("express-handlebars");
 const session = ("express-session")
 const path = require("path");
 //Requiring our passport configuration
-var passport = require("./config/passport");
-var passport = require("./models")
+//var passport = require("./config/passport");
+//var passport = require("./models")
 
 //Setting up port
 const app = express()
@@ -16,14 +16,19 @@ var PORT = process.env.PORT || 8080;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Uses sessions to keep track of our user's login status
-app.use(session({ secret: "MrSweetMeats", resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
+//app.use(session({ secret: "MrSweetMeats", resave: true, saveUninitialized: true }));
+//app.use(passport.initialize());
+//app.use(passport.session());
 
 app.use(express.static('public'))
 
-
+app.get("/", function(req, res) {
+    res.render("index");
+  });
 
 //Listening function
 app.listen(PORT, function() {
