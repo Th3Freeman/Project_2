baf3612ea126021465afa308c4282a18fe91d028
Th3freeman <thefrman345@gmail.com>
2019-12-03 12:37:27 -0500

    started to set up passport authentication

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 //Dependencies
 const express = require("express");
 const exphbs = require("express-handlebars");
+const session = ("express-session")
 const path = require("path");
+//Requiring our passport configuration
+var passport = require("./config/passport");
+var passport = require("./models")
 
 //Setting up port
 var PORT = process.env.PORT || 8080;
@@ -10,7 +14,10 @@ var PORT = process.env.PORT || 8080;
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
+// Uses sessions to keep track of our user's login status
+app.use(session({ secret: "MrSweetMeats", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
