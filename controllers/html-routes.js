c89063a75571443e2c35fe1697a410cb26acbafe
Dan Shelly <djshelly24@gmail.com>
2019-12-04 11:03:31 -0500

    Moved html routes to controller and used a router constant

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,20 +1,20 @@
+const express = require("express");
+const router = express.Router();
 
-const path = require("path");
 
-// Requiring our custom middleware for checking if a user is logged in
-const isAuthenticated = require("../config/middleware/isAuthenticated");
 
-module.exports = function(app) {
+router.get("/", function(req, res) {
+    res.render("LandingPage", { title: "LandingPage", css: "/stylesheets/Css/LandingPage.css" });
+});
 
-    app.get("/", function(req, res) {
-        res.render("LandingPage", { title: "LandingPage", css: "/stylesheets/Css/LandingPage.css" });
-    });
-    
-    app.get("/SnapShot", function(req, res) {
-        res.render("index", { title: "Snapshot", css: "/stylesheets/Css/index.css" });
-    });
-    
-    app.get("/UserSetUpBudget", function(req, res) {
-        res.render("UserSetUpBudget", { title: "Budget", css: "/stylesheets/Css/index.css" });
-    });
-};
+router.get("/SnapShot", function(req, res) {
+    res.render("index", { title: "Snapshot", css: "/stylesheets/Css/index.css" });
+});
+
+router.get("/UserSetUpBudget", function(req, res) {
+    res.render("UserSetUpBudget", { title: "Budget", css: "/stylesheets/Css/index.css" });
+});
+
+
+module.exports = router;
+
