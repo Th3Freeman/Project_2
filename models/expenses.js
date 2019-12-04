eb9476fe14995ed3db933de5234f7d852a3ebd70
cbush19 <cbush5400@gmail.com>
2019-12-04 13:50:41 -0500

    added foreign key

--- models/expenses.js
+++ models/expenses.js
@@ -3,11 +3,15 @@ module.exports = (sequelize, DataTypes) => {
   const Expenses = sequelize.define('Expenses', {
     category: DataTypes.INTEGER,
     desc: DataTypes.STRING,
-    amount: DataTypes.DECIMAL,
-    userId: DataTypes.INTEGER
+    amount: DataTypes.DECIMAL
   }, {});
   Expenses.associate = function(models) {
     // associations can be defined here
+    Expenses.associate = function (models) {
+      Expenses.belongsTo(models.User, {foreignKey: 'userId', targetKey: 'id'});
+    };
+  
+
   };
   return Expenses;
 };
